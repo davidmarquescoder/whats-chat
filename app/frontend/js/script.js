28cf86052440df29a8df5a7133cb946eb9f95337
David Marques <davidmarques99@hotmail.com>
2024-02-17 15:58:33 -0300

    Change server url

--- app/frontend/js/script.js
+++ app/frontend/js/script.js
@@ -111,7 +111,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://whatschat-ld0g.onrender.com")
     websocket.onmessage = processMessage
 }
 
